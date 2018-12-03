8d9f6f989e5f002721197002414600a1fef7eff2
Finn Terdal <fterdal@gmail.com>
2018-12-03 09:51:49 -0600

    code example starting point

--- react/index.js
+++ react/index.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const SingleRobot = (robot) => (
-  <div className="singleRobot">
-    <img src={`https://robohash.org/${robot.name}`} />
-    <p>{robot.name}</p>
-  </div>
-)
+const SingleRobot = ({robot}) => {
+  console.log('SINGLE ROBOT', robot)
+  return (
+    <div className="singleRobot">
+      <img src={`https://robohash.org/${robot.name}?size=150x150`} />
+      <p>{robot.name}</p>
+    </div>
+  )
+}
 
-const AllRobots = (robots = []) => (
-  <div>
-    {robots.map(robot => (
-      <SingleRobot key={robot.name} robot={robot} />
-    ))}
-  </div>
-)
+const AllRobots = ({ robots }) => {
+  console.log('ROBOTS', robots)
+  return (
+    <div className="allRobots">
+      {robots.map(robot => (
+        <SingleRobot key={robot.name} robot={robot} />
+      ))}
+    </div>
+  )
+}
 
-const App = () => (
-  <div>
-    <h1>Here are the robots:</h1>
-  </div>
-)
+const App = () => {
+  const robots = [
+    { name: 'finn' },
+    { name: 'priti' },
+    { name: 'collin' },
+    { name: 'ben' },
+  ]
+  return (
+    <div>
+      <h1>Here are the robots:</h1>
+      <AllRobots robots={robots} />
+    </div>
+  )
+}
 
 render(
   <App />,
