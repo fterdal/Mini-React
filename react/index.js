f7d708063bba037dd4e16fadef3fb984f1dd697e
Finn Terdal <fterdal@gmail.com>
2018-12-03 00:54:27 -0600

    getting started

--- react/index.js
+++ react/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => (
-  <div>
-    <h1>Hello from React!</h1>
-    <p>Riddle: We come at night without being fetched; we disappear by day without being stolen. What are we?</p>
-    <p style={{fontStyle: 'italic'}}>Pop open the developer tools to see the answer.</p>
+const SingleRobot = (robot) => (
+  <div className="singleRobot">
+    <img src={`https://robohash.org/${robot.name}`} />
+    <p>{robot.name}</p>
   </div>
 )
 
-console.log(`Answer:
-✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
-✨ ✨ ✨ Stars ✨ ✨ ✨
-✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ 
-`)
+const AllRobots = (robots = []) => (
+  <div>
+    {robots.map(robot => (
+      <SingleRobot key={robot.name} robot={robot} />
+    ))}
+  </div>
+)
+
+const App = () => (
+  <div>
+    <h1>Here are the robots:</h1>
+  </div>
+)
 
 render(
   <App />,
   document.getElementById('app')
 )
-
